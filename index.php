48e9e0b24e9563feadd96c7e7fdb6e8a1922d6fc
DevUsman-18 <mohammad_usman117@hotmail.co.uk>
2021-09-27 16:43:34 +0100

    layout complete, adjustments to be made

--- index.php
+++ index.php
@@ -4,11 +4,57 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Art Collection</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles.css" type="text/css">
     <link rel="stylesheet" href="normalize.css" type="text/css">
 </head>
 <body>
 
+<nav>
+    <div>
+        <h3>Art Collection</h3>
+    </div>
+</nav>
+
+<main>
+    <section>
+        <div>
+            <img src="images/canal.jpeg" />
+            <p> thiathiawhiatiaw</p>
+        </div>
+
+        <div>
+            <img src="images/flowers.jpeg" />
+            <p> thiathiawhiatiaw</p>
+        </div>
+
+        <div>
+            <img src="images/goingwest.jpeg" />
+            <p> thiathiawhiatiaw</p>
+        </div>
+
+        <div>
+            <img src="images/haywain.jpeg" />
+            <p> thiathiawhiatiaw</p>
+        </div>
+
+        <div>
+            <img src="images/naples.jpeg" />
+            <p> thiathiawhiatiaw</p>
+        </div>
+
+        <div>
+            <img src="images/paintedhall.jpeg" />
+            <p> thiathiawhiatiaw</p>
+        </div>
+
+    </section>
+</main>
+
+<footer>
+
+
+</footer>
 
 </body>
 </html>
