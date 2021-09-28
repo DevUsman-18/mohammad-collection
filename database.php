fa5364d2794d8e33d6af289f78d4faea3045b1e0
DevUsman-18 <mohammad_usman117@hotmail.co.uk>
2021-09-28 14:24:41 +0100

    display function written

--- database.php
+++ database.php
@@ -9,12 +9,15 @@ $query = $db->prepare(" SELECT `artist`, `year-made`, `painting-name`,`image-lin
 $query->execute();
 $results = $query->fetchAll();
 
+echo '<pre>';
+var_dump($results);
+echo '</pre>';
 
-function displayDB(array $results) {
+function displayDB(array $results) : string {
     $dbResult = ' ';
     foreach($results as $result){
        $dbResult .= '<div>'. '<img src="images/' . $result['image-link'] . '" alt="a painting of  '
-           . $result['painting-name'] .' " /> <p>'
+           . $result['painting-name'] .' " /><p>'
            . $result['artist'] . '<br>' . $result['painting-name'] . '<br>' . $result['year-made'] .  '</p></div>';
     }
     return $dbResult;
---
+++ tests/database.php
@@ -0,0 +1,23 @@
+<?php
+
+require './database.php';
+
+use PHPUnit\Framework\TestCase;
+
+class database extends TestCase {
+    //success test
+    public function testDisplayDB(){
+    $input = ["artist"=>"osaias",
+        "year-made" => 2000,
+        "painting-name" => "lmao",
+        "image-link"=> "foo.bar"
+    ] ;
+
+    $expected = '<div>'. '<img src="images/' . $result['image-link'] . '" alt="a painting of  '
+            . $result['painting-name'] .' " /><p>'
+            . $result['artist'] . '<br>' . $result['painting-name'] . '<br>' . $result['year-made'] .  '</p></div>';
+
+    $result = displayDB($input);
+    $this->assertContains("year-made", $result);
+    }
+}
